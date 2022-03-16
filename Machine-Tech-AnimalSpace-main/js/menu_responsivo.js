326924656c4e7c270c3bc25415829b9f8efe7efd
Guilherme Muraro <101333472+guilh9rme@users.noreply.github.com>
2022-03-16 11:34:18 -0300

    Add files via upload

---
+++ Machine-Tech-AnimalSpace-main/js/menu_responsivo.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.mobile-menu').click(function(){
+		$('.mobile-menu').find('ul').slideToggle();
+	})
+})
+
+$(function(){
+	$('.mobile-menu-branco').click(function(){
+		$('.mobile-menu-branco').find('ul').slideToggle();
+	})
+})
